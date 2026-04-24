3037bc7c77d022af55eef41d2e704da55f508d69
Lucas Diedrich <diedrich@biochem.mpg.de>
2026-04-24 07:31:10 +0200

    [Refactor] Move query logic to query accessor

--- src/mulink/query.py
+++ src/mulink/query.py
@@ -1,7 +1,8 @@
 """Query an adjacency matrix"""
 
-from collections.abc import Iterable
+from collections.abc import Callable, Iterable
 
+import mudata as md
 import numpy as np
 from scipy.sparse import csr_matrix
 
@@ -50,3 +51,85 @@ def get_ancestors(vertices: int | Iterable[int], adjacency_matrix: csr_matrix) -
 
     # N:M mapping might yield redundant features - only return unique features
     return np.unique(rows)
+
+
+class QueryAccessor:
+    """Query functionality for mulink"""
+
+    def __init__(self, link):
+        self._link = link
+        self._obj = self._link._obj
+
+    def _query(
+        self,
+        query_func: Callable[[np.ndarray, csr_matrix], np.ndarray],
+        features: str | list[str],
+        *,
+        key: str = "feature_mapping",
+        include_self: bool = True,
+    ) -> md.MuData:
+        adjacency_matrix = self._obj.varp[key]
+
+        features = [features] if isinstance(features, str) else features
+        query_indices = self._obj.var_names.get_indexer(features)
+
+        result_indices = query_func(vertices=query_indices, adjacency_matrix=adjacency_matrix)
+
+        if include_self:
+            result_indices = np.concatenate([query_indices, result_indices])
+
+        return self._obj[:, self._obj.var_names[result_indices]]
+
+    def descendants(
+        self,
+        features: str | list[str],
+        *,
+        key: str = "feature_mapping",
+        include_self: bool = True,
+    ) -> md.MuData:
+        """Get direct descendants of features
+
+        Examples
+        --------
+
+        .. code-block:: python
+
+            mdata = mulink.simulate.hierarchical_mudata(n_mod=3)
+
+            mdata.link.query.descendants(features="mod0-0")
+            mdata.link.query.descendants(features=["mod0-0", "mod0-1"])
+
+        """
+        return self._query(
+            query_func=get_descendants,
+            features=features,
+            key=key,
+            include_self=include_self,
+        )
+
+    def ancestors(
+        self,
+        features: str | list[str],
+        *,
+        key: str = "feature_mapping",
+        include_self: bool = True,
+    ) -> md.MuData:
+        """Get direct ancestors of features
+
+        Examples
+        --------
+
+        .. code-block:: python
+
+            mdata = mulink.simulate.hierarchical_mudata(n_mod=3)
+
+            mdata.link.query.ancestors(features="mod2-0")
+            mdata.link.query.ancestors(features=["mod2-0", "mod2-1"])
+
+        """
+        return self._query(
+            query_func=get_ancestors,
+            features=features,
+            key=key,
+            include_self=include_self,
+        )

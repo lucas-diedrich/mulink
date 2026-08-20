e92c209a39fd7f3012415f0a4f22d852cd2d748b
Lucas Diedrich <diedrich@biochem.mpg.de>
2026-08-20 10:08:29 +0200

    [test] Update query helper function tests so that all ancestors/descendants are retrieved

--- tests/test_query.py
+++ tests/test_query.py
@@ -1,7 +1,69 @@
 import mudata as md
+import numpy as np
+import pytest
 from numpy.testing import assert_array_equal
 
 import mulink  # noqa: F401 — registers the namespace
+from mulink.query import get_ancestors, get_descendants
+
+
+@pytest.fixture
+def simple_adjacency_matrix() -> np.ndarray:
+    """Simple adjacency matrix
+
+    Encodes the graph:
+
+    A -> B
+    A -> C
+    B -> D
+    B -> E
+    C -> E
+    """
+    return np.array(
+        [
+            # A, B, C, D, E
+            [0, 1, 1, 0, 0],  # A
+            [0, 0, 0, 1, 1],  # B
+            [0, 0, 0, 0, 1],  # C
+            [0, 0, 0, 0, 0],  # D
+            [0, 0, 0, 0, 0],  # E
+        ]
+    )
+
+
+class TestGetDescendants:
+    @pytest.mark.parametrize(
+        ("query_indices", "expected_result_indices"),
+        [
+            (0, np.array([0, 1, 2, 3, 4])),  # A->B, A->C, B->D, B->E, C->E; search includes self
+            (4, np.array([4])),  # Does not map to any other node. Search includes self
+            ([0, 1], np.array([0, 1, 2, 3, 4])),  # A includes all, B does not contribute
+            ([1], np.array([1, 3, 4])),  # B->D, C->E; search includes self
+            ([3, 4], np.array([3, 4])),  # Do not map to anything, only includes self
+        ],
+    )
+    def test_get_descendants(self, simple_adjacency_matrix, query_indices, expected_result_indices) -> None:
+        """Test that the function correctly queries the adjacency matrix"""
+        result = get_descendants(vertices=query_indices, adjacency_matrix=simple_adjacency_matrix)
+
+        assert_array_equal(result, expected_result_indices)
+
+
+class TestGetAncestors:
+    @pytest.mark.parametrize(
+        ("query_indices", "expected_result_indices"),
+        [
+            (0, np.array([0])),  # No ancestors, get self
+            (4, np.array([0, 1, 2, 4])),  # E <- C; E <- B; E <- B <- A; E <- C <- A
+            ([4], np.array([0, 1, 2, 4])),
+            ([1, 2], np.array([0, 1, 2])),
+        ],
+    )
+    def test_get_ancestors(self, simple_adjacency_matrix, query_indices, expected_result_indices) -> None:
+        """Test that the function correctly queries the adjacency matrix"""
+        result = get_ancestors(vertices=query_indices, adjacency_matrix=simple_adjacency_matrix)
+
+        assert_array_equal(result, expected_result_indices)
 
 
 class TestQueryDescendants:
